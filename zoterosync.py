57d83655299191c483693eec6306a4c92d1f019f
Arjan Mossel <arjanmossel@gmail.com>
2023-10-06 17:53:09 +0200

    Refactor

--- zoterosync.py
+++ zoterosync.py
@@ -8,145 +8,169 @@ import sqlite3
 from dotenv import load_dotenv
 from pyzotero import zotero
 
-#%% Logging config
 logging.basicConfig(level=logging.INFO)
 
-#%% Zotero library configuration
-load_dotenv()
-LIBRARY_ID = os.getenv('ZOTERO_LIBRARY_ID')
-LIBRARY_TYPE = os.getenv('ZOTERO_LIBRARY_TYPE')
-API_KEY = os.getenv('ZOTERO_API_KEY')
 
-#%% Zotero instance
-z = zotero.Zotero(LIBRARY_ID, LIBRARY_TYPE, API_KEY, preserve_json_order=True)
+def load_configuration():
+    load_dotenv()
+    config = {
+        'projroot': pathlib.Path(__file__).parents[0].resolve(),
+        'dbname': os.getenv('DBNAME'),
+        'library_id': os.getenv('ZOTERO_LIBRARY_ID'),
+        'library_type': os.getenv('ZOTERO_LIBRARY_TYPE'),
+        'api_key': os.getenv('ZOTERO_API_KEY'),
+    }
 
-#%% SQLite database configuration
-PROJROOT = pathlib.Path(__file__).parents[0].resolve()
-DBNAME = os.environ['DBNAME']
-DBPATH = os.path.join(PROJROOT, DBNAME)
-conn = sqlite3.connect(DBPATH)
-conn.row_factory = sqlite3.Row
-cur = conn.cursor()
+    return config
 
-def prepare_db():
-    cur.execute(
-    '''CREATE TABLE IF NOT EXISTS publications (
-        key TEXT PRIMARY KEY
-        , content JSON
-        );
-    ''')
-    cur.execute(
-    '''CREATE TABLE IF NOT EXISTS annotations (
-        key TEXT PRIMARY KEY
-        , content JSON
-        );
-    ''')
-    cur.execute(
-    '''CREATE TABLE IF NOT EXISTS sync (
-        library TEXT PRIMARY KEY
-        , library_version INTEGER
-        );
-    ''')
-    conn.commit()
 
-prepare_db()
+def create_tables(conn, cur):
+    try:
+        cur.execute(
+        '''CREATE TABLE IF NOT EXISTS publications (
+            key TEXT PRIMARY KEY
+            , content JSON
+            );
+        ''')
+        cur.execute(
+        '''CREATE TABLE IF NOT EXISTS annotations (
+            key TEXT PRIMARY KEY
+            , content JSON
+            );
+        ''')
+        cur.execute(
+        '''CREATE TABLE IF NOT EXISTS sync (
+            library TEXT PRIMARY KEY
+            , library_version INTEGER
+            );
+        ''')
+        conn.commit()
+    except sqlite3.Error as e:
+        logging.error(f"Database error: {e}")
+    except Exception as e:
+        logging.error(f"Exception: {e}")
 
-#%%
-def sync_publications(since):
+
+def setup_database(dbpath):
+    conn = sqlite3.connect(dbpath)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    create_tables(conn, cur)
+
+    return conn, cur
+
+
+def get_zotero_instance(config):
+    return zotero.Zotero(
+        config['library_id'],
+        config['library_type'],
+        config['api_key'],
+        preserve_json_order=True
+    )
+
+
+def sync_publications(conn, cur, zot, since):
     """ Fetches publications which have been updated since the given library version.
     Saves the publications to database.
-    Returns 1 if successful, 0 otherwise.
     """
     # All publications should be placed top-level (not in collections)
     # Non-publication items might exist as well but can be ignored.
     # (There's no way to filter on bibliographic items in general directly?)
-    items = z.everything(z.top(since=since))
+    items = zot.everything(zot.top(since=since))
     if items:
-        for i, item in enumerate(items):
+        for item in items:
             key = item['key']
             obj = item
-            cur.execute("""
-                INSERT INTO publications (key, content)
-                VALUES (:key, :content)
-                ON CONFLICT (key) DO UPDATE SET content = :content
-                ;""", {'key': key, 'content': json.dumps(obj)}
-                )
-            conn.commit()
-        logging.info(f"Updated {i} publications")
-        return 1
+            try:
+                cur.execute("""
+                    INSERT INTO publications (key, content)
+                    VALUES (:key, :content)
+                    ON CONFLICT (key) DO UPDATE SET content = :content
+                    ;""", {'key': key, 'content': json.dumps(obj)}
+                    )
+                conn.commit()
+            except sqlite3.Error as e:
+                logging.error(f"Database error: {e}")
+        logging.info(f"Updated {len(items)} publications")
     else:
         logging.info(f"No new publications found")
-        return 0
 
 
-def sync_annotations(since):
+def sync_annotations(conn, cur, zot, since):
     """ Fetches items of type 'annotation' which have been updated since the given library version.
     Saves the annotations to database.
-    Returns 1 if successful, 0 otherwise.
     """
     # TODO: use Zotero.follow() or iterfollow() methods
     # https://pyzotero.readthedocs.io/en/latest/#the-follow-and-everything-methods
-    items = z.everything(z.items(itemType='annotation', since=since))
+    items = zot.everything(zot.items(itemType='annotation', since=since))
     if items:
         for i, item in enumerate(items):
             key = item['key']
             obj = item
-            cur.execute("""
-                INSERT INTO annotations (key, content)
-                VALUES (:key, :content)
-                ON CONFLICT (key) DO UPDATE SET content = :content
-                ;""", {'key': key, 'content': json.dumps(obj)}
-                )
-            conn.commit()
-        logging.info(f"Updated {i + 1} annotations")
-        return 1
+            try:
+                cur.execute("""
+                    INSERT INTO annotations (key, content)
+                    VALUES (:key, :content)
+                    ON CONFLICT (key) DO UPDATE SET content = :content
+                    ;""", {'key': key, 'content': json.dumps(obj)}
+                    )
+                conn.commit()
+            except sqlite3.Error as e:
+                logging.error(f"Database error: {e}")
+        logging.info(f"Updated {len(items)} publications")
     else:
         logging.info(f"No new annotations found")
-        return 0
 
 
-def get_local_library_version():
-    """ Returns the last synced library version from the local database
+def get_local_library_version(cur, zot):
+    """ Returns the last synced library version from the local database.
     """
-    cur.execute("""
-        SELECT library_version
-        FROM sync
-        WHERE library=:library_id
-        ;""", {'library_id': z.library_id}
-    )
-    row = cur.fetchone()
-    local_library_version = row[0] if row else -1
-    logging.info(f"local_library_version: {local_library_version}")
-    return local_library_version if local_library_version else 0
+    try:
+        cur.execute("""
+            SELECT library_version
+            FROM sync
+            WHERE library=:library_id
+            ;""", {'library_id': zot.library_id}
+        )
+        row = cur.fetchone()
+        local_library_version = row[0] if row else -1
+        logging.info(f"local_library_version: {local_library_version}")
+        return local_library_version if local_library_version else 0
+    except sqlite3.Error as e:
+        logging.error(f"Database error: {e}")
 
 
-def sync():
-    zotero_library_version = z.last_modified_version()
-    local_library_version = get_local_library_version()
+def sync(conn, cur, zot):
+    zotero_library_version = zot.last_modified_version()
+    local_library_version = get_local_library_version(cur, zot)
     if zotero_library_version > local_library_version:
-        p = sync_publications(since=local_library_version)
-        a = sync_annotations(since=local_library_version)
-        if p and a:
+        sync_publications(conn, cur, zot, since=local_library_version)
+        sync_annotations(conn, cur, zot, since=local_library_version)
+        try:
             cur.execute('''
             INSERT INTO sync (library, library_version)
             VALUES (:library_id, :library_version)
             ON CONFLICT (library) DO UPDATE SET library_version = :library_version
             ;''', {
                     'library_version': zotero_library_version,
-                    'library_id': z.library_id
+                    'library_id': zot.library_id
                 }
             )
             conn.commit()
             logging.info("Sync successful")
-        else:
-            logging.warning("Sync incomplete")
+        except sqlite3.Error as e:
+            logging.error(f"Database error: {e}")
     else:
         logging.info("Local library up to date")
 
-#%%
-sync()
 
-#%%
-logging.info(
-    f"Local library version: {get_local_library_version()}, Remote library version: {z.last_modified_version()}",
-)
+def main():
+    config = load_configuration()
+    dbpath = os.path.join(config['projroot'], config['dbname'])
+    conn, cur = setup_database(dbpath)
+    zot = get_zotero_instance(config)
+    sync(conn, cur, zot)
+
+
+if __name__ == "__main__":
+    main()
