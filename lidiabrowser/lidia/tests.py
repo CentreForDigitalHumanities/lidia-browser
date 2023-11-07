7e2f7933579eef95a01da7402aba859e9b54ce24
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-11-07 11:32:08 +0100

    Add admin command for group creation

--- lidiabrowser/lidia/tests.py
+++ lidiabrowser/lidia/tests.py
@@ -1,2 +1,19 @@
-def test_dummy():
-    pass
+import pytest
+from django.contrib.auth.models import Group, Permission
+from django.core.management import call_command
+
+
+@pytest.mark.django_db
+class TestInitgroups:
+    def test_basic(self):
+        call_command("initgroups")
+        view_all = Group.objects.get(name="view_all")
+        viewpublicationpermission = Permission.objects.get_by_natural_key(
+            "view_publication", "lidia", "publication"
+        )
+        assert view_all.permissions.contains(viewpublicationpermission)
+
+    def test_double(self):
+        # Calling twice on the same database should be fine
+        call_command("initgroups")
+        call_command("initgroups")
