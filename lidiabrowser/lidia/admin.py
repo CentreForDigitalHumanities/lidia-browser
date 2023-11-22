aa334f8670a8b8deae81d139053d94cdeee4b5df
Tijmen Baarda <tijmenbaarda@gmail.com>
2023-11-22 13:39:54 +0100

    Only show implementation details to superuser

--- lidiabrowser/lidia/admin.py
+++ lidiabrowser/lidia/admin.py
@@ -1,4 +1,6 @@
+from typing import List, Type
 from django.contrib import admin
+from django.http import HttpRequest
 
 from .models import (
     Annotation,
@@ -48,41 +50,11 @@ class AnnotationAdmin(admin.ModelAdmin):
         "termgroups__articleterm__term",
         "termgroups__category__category",
     ]
-    fieldsets = [
-        (
-            None, {
-                "fields": [
-                    "parent_attachment",
-                    "argname",
-                    "full_quotation",
-                    "description",
-                    "page_range",
-                    "page_range_in_pdf",
-                    "arglang",
-                ],
-            }
-        ), (
-            "Relation to other annotation", {
-                "fields": [
-                    "relation_type",
-                    "relation_to",
-                ],
-            }
-        ), (
-            "Technical details", {
-                "fields": [
-                    "lidia_id",
-                    "zotero_annotation",
-                ]
-            }
-        )
-    ]
     readonly_fields = [
         "page_range",
         "page_range_in_pdf",
         "full_quotation"
     ]  # Necessary for callables
-    change_form_template = "lidia/change_form_annotation.html"
 
     @admin.display(
         ordering="argname",
@@ -122,6 +94,47 @@ class AnnotationAdmin(admin.ModelAdmin):
     def page_range_complete(self, obj: Annotation):
         return f"{obj.page_range} ({obj.page_range_in_pdf})"
 
+    def get_fieldsets(self, request: HttpRequest, obj=None):
+        fieldsets = [
+            (
+                None, {
+                    "fields": [
+                        "parent_attachment",
+                        "argname",
+                        "full_quotation",
+                        "description",
+                        "page_range",
+                        "page_range_in_pdf",
+                        "arglang",
+                    ],
+                }
+            ), (
+                "Relation to other annotation", {
+                    "fields": [
+                        "relation_type",
+                        "relation_to",
+                    ],
+                }
+            ), (
+                "Technical details", {
+                    "fields": [
+                        "lidia_id",
+                    ]
+                }
+            )
+        ]
+        if request.user.is_superuser:  # type: ignore
+            # Only show link to annotation in sync to superuser
+            fieldsets[2][1]["fields"].append("zotero_annotation")
+        return fieldsets
+
+    def get_inlines(self, request: HttpRequest, obj=None):
+        inlines: List[Type[admin.TabularInline]] = [TermGroupInline]
+        if request.user.is_superuser:  # type: ignore
+            # Only show continuation annotations to superuser because
+            # for normal users this distinction is irrelevant
+            inlines.append(ContinuationInline)
+        return inlines
 
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ["zotero_publication", "attachment_id", "title"]
